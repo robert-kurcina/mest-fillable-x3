cec22e498daadae88245515489be8e0786f27589
Robert Kurcina <kitrok@gmail.com>
2025-05-01 21:04:59 -0700

    updated the /build/output.js

--- build/output.js
+++ build/output.js
@@ -1,81 +1,4 @@
-//MEST.acrojs-polyfills
-
-/**
- * A polyfile for Object.keys(object);
- */
-if (!Object.keys) {
-  Object.keys = (function () {
-    var hasOwnProperty = Object.prototype.hasOwnProperty,
-        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
-        dontEnums = [
-          'toString',
-          'toLocaleString',
-          'valueOf',
-          'hasOwnProperty',
-          'isPrototypeOf',
-          'propertyIsEnumerable',
-          'constructor'
-        ],
-        dontEnumsLength = dontEnums.length;
-
-    return function (obj) {
-      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
-
-      var result = [];
-
-      for (var prop in obj) {
-        if (hasOwnProperty.call(obj, prop)) result.push(prop);
-      }
-
-      if (hasDontEnumBug) {
-        for (var i=0; i < dontEnumsLength; i++) {
-          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
-        }
-      }
-      return result;
-    };
-  })();
-}
-
-/**
- * A polyfil for stringify(object);
- * @param {} data 
- * @returns string
- * @see https://gist.github.com/rajatjain-21/02e2c5a30cf9d0190cb5503a25417fd1
- */
-// This implementation does not work with Symbols, BigInts
-function stringify(data) {
-  if (data === undefined)
-    return undefined
-  if (data === null)
-    return 'null'
-  if (data.toString() === "NaN")
-    return 'null'
-  if (data === Infinity)
-    return 'null'
-  if (data.constructor === String)
-    return '"' + data.replace(/"/g, '\\"') + '"'
-  if (data.constructor === Number)
-    return String(data)
-  if (data.constructor === Boolean)
-    return data ? 'true' : 'false'
-  if (data.constructor === Array)
-    return '[' + data.reduce((acc, v) => {
-      if (v === undefined || v === NaN || v === Infinity)
-        return [...acc, 'null']
-      else
-        return [...acc, stringify(v)]
-    }, []).join(',') + ']'
-  if (data.constructor === Object)
-    return '{' + Object.keys(data).reduce((acc, k) => {
-      if (data[k] === undefined)
-        return acc
-      else
-        return [...acc, stringify(k) + ':' + stringify(data[k])]
-    }, []).join(',') + '}'
-
-    return '{}'
-}//MEST.data-controls
+//data-controls.js
 
 var SUFFIX_LIST = ["_a", "_b", "_c"];
 var BP_SUBFIELDS = ["fBP", "BP_1", "BP_2", "BP_3", "BP_4", "BP_item_1", "BP_item_2"];
@@ -167,7 +90,7 @@ var TOGGLE_STATUS = {
    "noView": 3
 }
 
-//MEST.data-fillable-items.js
+//data-fillable-items.js
 
 var ADJUST_AVERAGE = -57;
 var AVERAGE_ATTR = 2;
@@ -878,7 +801,84 @@ MASTER_MENU.ARMORS = {
       "traits": "[Laden 3][Lumbering]. Deflect.",
       "BP": 15
    }
-}//MEST.functions
+}//functions-polyfills.js
+
+/**
+ * A polyfile for Object.keys(object);
+ */
+if (!Object.keys) {
+  Object.keys = (function () {
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function (obj) {
+      if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+
+      var result = [];
+
+      for (var prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) result.push(prop);
+      }
+
+      if (hasDontEnumBug) {
+        for (var i=0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+        }
+      }
+      return result;
+    };
+  })();
+}
+
+/**
+ * A polyfil for stringify(object);
+ * @param {} data 
+ * @returns string
+ * @see https://gist.github.com/rajatjain-21/02e2c5a30cf9d0190cb5503a25417fd1
+ */
+// This implementation does not work with Symbols, BigInts
+function stringify(data) {
+  if (data === undefined)
+    return undefined
+  if (data === null)
+    return 'null'
+  if (data.toString() === "NaN")
+    return 'null'
+  if (data === Infinity)
+    return 'null'
+  if (data.constructor === String)
+    return '"' + data.replace(/"/g, '\\"') + '"'
+  if (data.constructor === Number)
+    return String(data)
+  if (data.constructor === Boolean)
+    return data ? 'true' : 'false'
+  if (data.constructor === Array)
+    return '[' + data.reduce((acc, v) => {
+      if (v === undefined || v === NaN || v === Infinity)
+        return [...acc, 'null']
+      else
+        return [...acc, stringify(v)]
+    }, []).join(',') + ']'
+  if (data.constructor === Object)
+    return '{' + Object.keys(data).reduce((acc, k) => {
+      if (data[k] === undefined)
+        return acc
+      else
+        return [...acc, stringify(k) + ':' + stringify(data[k])]
+    }, []).join(',') + '}'
+
+    return '{}'
+}//functions-process.js
 
 function calcBPTotal(bpFields, suffix){
    var sum = 0;
@@ -1221,13 +1221,21 @@ function runBuild(){
    performBuild(BUILD_TARGETS_BY_NAME, SUFFIX_LIST);
 }
 
+function prepareForm(){
+   var btnBuild = this.getField("btn_build") || {};
+   var btnCalculate = this.getField("btn_calculate") || {};
+   var btnReset = this.getField("btn_reset") || {};
+
+   btnBuild.setAction("MouseUp", "runBuild();");
+   btnCalculate.setAction("MouseUp", "runCalculate();");
+   btnReset.setAction("MouseUp", "runReset();");
+}
+
 function runInitialize(){
    showEverything();
    initializeGlobal();
+   prepareForm();
 }
 
-//runInitialize(); invoke at end of INITIALIZE button
-//runBuild(); invoke via BUILD button
-//runCalculate(); invoke via CALCULATE button
-//runReset(); invoke via RESET button
+
 
